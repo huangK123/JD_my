81e909519d13cb02bc8c6be024bb6254c945ce10
huangliming <13314504191@163.com>
2018-09-13 17:58:41 +0800

    添加一行代码，测试能否push成功

--- src/main/java/com/test/TestV1P1.java
+++ src/main/java/com/test/TestV1P1.java
@@ -12,15 +12,19 @@ public class TestV1P1 {
 
 
     public static void main(String args[]){
-        System.out.println("test1");
-        List<String> list1 = new ArrayList<>();
-        list1.add("test1");
-        list1.add("test2");
-        list1.add("test3");
-        list1.add("test4");
-        for(String str : list1){
-            System.out.println(str);
+        int n = 10;
+        for(int i=0;i<n;n++){
+            System.out.println(n);
         }
-        System.out.println("test1line2");
+//        System.out.println("test1");
+//        List<String> list1 = new ArrayList<>();
+//        list1.add("test1");
+//        list1.add("test2");
+//        list1.add("test3");
+//        list1.add("test4");
+//        for(String str : list1){
+//            System.out.println(str);
+//        }
+//        System.out.println("test1line2");
     }
 }
