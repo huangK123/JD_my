3d6747453b9e947a569379859b8bd373cf36aef4
huangliming <13314504191@163.com>
2018-09-13 18:06:34 +0800

    自测git

--- src/main/java/com/test/TestV1P1.java
+++ src/main/java/com/test/TestV1P1.java
@@ -16,15 +16,15 @@ public class TestV1P1 {
         for(int i=0;i<n;n++){
             System.out.println(n);
         }
-//        System.out.println("test1");
-//        List<String> list1 = new ArrayList<>();
-//        list1.add("test1");
-//        list1.add("test2");
-//        list1.add("test3");
-//        list1.add("test4");
-//        for(String str : list1){
-//            System.out.println(str);
-//        }
-//        System.out.println("test1line2");
+        System.out.println("test1");
+        List<String> list1 = new ArrayList<>();
+        list1.add("test1");
+        list1.add("test2");
+        list1.add("test3");
+        list1.add("test4");
+        for(String str : list1){
+            System.out.println(str);
+        }
+        System.out.println("test1line2");
     }
 }
